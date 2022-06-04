8278a4a0d063c7f5d1cd448facac68dd439f6ab0
angela <a0923356775@gmail.com>
2022-06-04 22:21:08 +0800

    input

--- direct_mapped_cache.cpp
+++ direct_mapped_cache.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+#include <string>
 #include "direct_mapped_cache.h"
 #include "string"
 
@@ -9,12 +11,15 @@ float direct_mapped(string filename, int block_size, int cache_size)
     int hit_num = 0;
     
     /*write your code HERE*/
-    printf("in direct_mapped\n");
+    fstream inf;
+    inf.open(filename);
+    string instr;
+
+    while(getline(inf, instr)){
+        cout<< instr <<endl;
     
     
-    
-    
-    
+    }
   
     return (float)hit_num/total_num;
 }
