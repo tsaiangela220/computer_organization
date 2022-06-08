f1902cb18a33920aa3fc050f0a9b8d22af3c5d90
angela <a0923356775@gmail.com>
2022-06-08 23:09:35 +0800

    double check

--- set_associative_cache.cpp
+++ set_associative_cache.cpp
@@ -2,11 +2,12 @@
 #include <cstring>
 #include <cmath>
 #include "set_associative_cache.h"
-#include "direct_mapped_cache.cpp"
 #include "string"
 
 using namespace std;
 
+string Hex2bin(string);
+int Bin2dec(string);
 struct set_cache{
     bool valid;
     int time_lastUse=0;
@@ -41,7 +42,7 @@ float set_associative(string filename, int way, int block_size, int cache_size)
         cout<< "tag_bitNum:    "<< tag_bitNum<< endl;//
         
         cout<< "address:       "<< address <<endl;//
-        address_bin = hex2bin(address);    
+        address_bin = Hex2bin(address);    
         cout<< address_bin <<endl;//
 
         //00 000 00
@@ -50,7 +51,7 @@ float set_associative(string filename, int way, int block_size, int cache_size)
         for(int i= offset_bitNum+index_bitNum; i< address_bin.length(); i++) 
             tag += address_bin[i];
         cout<< "index_bin: "<< index_bin<< endl;//
-        index_dec = bin2dec(index_bin);
+        index_dec = Bin2dec(index_bin);
         cout<< "index_dec: "<< index_dec<< endl;//
         cout<< "tag:       "<< tag<<endl;//
 
@@ -69,4 +70,46 @@ float set_associative(string filename, int way, int block_size, int cache_size)
     }
  
     return (float)hit_num/total_num;
+}
+
+int Bin2dec(string bin){
+    int i=0, dec=0, base=1;
+
+    while(i < bin.length()){
+        dec += base*(bin[i]-48); //ascii code of 0 is 48
+        base*=2;
+        i++;
+    }
+
+    return dec;
+}
+string Hex2bin(string hex)
+{
+    string bin;
+    int i=hex.length()-1;
+
+    //e.g.MSB bf979d20 LSB(index0)
+    while(i > -1){
+        switch(hex[i--]){
+            case '0': bin += "0000"; break;//index3 0000 index0
+            case '1': bin += "1000"; break;//0001
+            case '2': bin += "0100"; break;//0010
+            case '3': bin += "1100"; break;//0011
+            case '4': bin += "0010"; break;//0100
+            case '5': bin += "1010"; break;//0101
+            case '6': bin += "0110"; break;//0110   
+            case '7': bin += "1110"; break;//0111 
+            case '8': bin += "0001"; break;//1000
+            case '9': bin += "1001"; break;//1001
+            case 'a': bin += "0101"; break;//1010
+            case 'b': bin += "1101"; break;//1011
+            case 'c': bin += "0011"; break;//1100
+            case 'd': bin += "1011"; break;//1101
+            case 'e': bin += "0111"; break;//1110
+            case 'f': bin += "1111"; break;//1111 
+        }
+    }
+    while(bin.length() < 32) bin += '0';
+
+    return bin;
 }
